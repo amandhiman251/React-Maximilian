c29edd439dc0c613e187a34e5f27b4f08ce6370a
Aman Dhiman <amandhiman251@gmail.com>
2020-11-02 01:00:43 +0530

    creating first functional component

--- src/container/App.js
+++ src/container/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from '../component/Person/Person';
 
 
 class App extends Component {
 
     render(){
-        //    return(
-        //        <div className = "App">
-        //           <h1>Hi, I am a React App</h1>
-        //        </div>
-        //    );
-        return React.createElement('div', {className:"App"}, React.createElement('h1', null, 'Hi, I am a React App'))
+        return(
+            <div class = "App">
+                <h1>Hi, I am a React App</h1>
+                <p>This is really working!</p>
+                <Person />
+            </div>
+           );
     
     }
 }
