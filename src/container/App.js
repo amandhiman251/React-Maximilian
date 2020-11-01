bcf9153aff21ea34f27b34dafec61843c2f05f55
Aman Dhiman <amandhiman251@gmail.com>
2020-11-02 00:20:39 +0530

    behind the scene how jsx rendered

--- src/container/App.js
+++ src/container/App.js
@@ -6,11 +6,12 @@ import './App.css';
 class App extends Component {
 
     render(){
-           return(
-               <div className = "App">
-                  <h1>Hi, I am a React App</h1>
-               </div>
-           );
+        //    return(
+        //        <div className = "App">
+        //           <h1>Hi, I am a React App</h1>
+        //        </div>
+        //    );
+        return React.createElement('div', {className:"App"}, React.createElement('h1', null, 'Hi, I am a React App'))
     
     }
 }
