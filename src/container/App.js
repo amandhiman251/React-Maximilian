cb70a603098fefeac62a136dfd4479433cd7dc91
Aman Dhiman <amandhiman251@gmail.com>
2020-11-02 19:40:54 +0530

    passing method/function arguments between components

--- src/container/App.js
+++ src/container/App.js
@@ -13,24 +13,24 @@ class App extends Component {
          otherstate :'some other value'  
      }
 
-    switchNameHandler = () => {
+    switchNameHandler = (newname) => {
         this.setState ({
              person : [
-                 { name : "Karan", age : "30"}, 
+                 { name : newname, age : "30"}, 
                  {name : "Sweety", age : "27"}, 
                  {name : "julie", age : "50"} 
              ]  }
         )
     };
-
+    
     render(){
         return(
             <div className = "App">
                 <h1>Hi, I am a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}> Switch Name </button>
+                <button onClick={ this.switchNameHandler.bind(this, "usha")}> Switch Name </button>
                 <Person name = {this.state.person[0].name} age = {this.state.person[0].age} />
-                <Person name = {this.state.person[1].name} age = {this.state.person[1].age} >My Hobbies: Racing </Person>
+                <Person name = {this.state.person[1].name} age = {this.state.person[1].age} click={this.switchNameHandler.bind(this, "Balbir")} >My Hobbies: Racing </Person>
                 <Person name = {this.state.person[2].name} age ={this.state.person[1].age} />
             </div>
            );
